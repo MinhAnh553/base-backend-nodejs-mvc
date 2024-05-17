f5b156fcca508d08fac5a27a3af9a52cc36a7bcd
MinhAnhIT <anhhoclaptrinh@gmail.com>
2024-05-17 07:58:37 +0700

    Thêm Slug

--- models/exampleModel.js
+++ models/exampleModel.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+mongoose.plugin(slug);
 
-const exampleSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    price: Number,
-    discountPercentage: Number,
-    stock: Number,
-    thumbnail: String,
-    status: String,
-    position: Number,
-    deleted: Boolean,
-    deletedAt: Date,
-});
+const exampleSchema = new mongoose.Schema(
+    {
+        title: String,
+        description: String,
+        slug: { type: String, slug: 'title', unique: true },
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: Date,
+    },
+    {
+        timestamps: true,
+    }
+);
 
 const Example = mongoose.model('Tên model', exampleSchema, 'tên table');
 
